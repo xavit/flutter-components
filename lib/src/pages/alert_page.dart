7d30c75a6ddc0b2657923332f31acd763839d978
Javier Claros <jclaros@afp-futuro.com>
2019-12-05 11:17:17 -0400

    Se agrego Una 1 para todos los botones Navigator

---
+++ lib/src/pages/alert_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class AlertPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Alert Page'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.arrow_back),
+        onPressed: () {
+          Navigator.pop(context);
+        },
+      ),
+      body: Center(
+        child: Text('Hi'),
+      ),
+    );
+  }
+}
--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_components/src/pages/alert_page.dart';
 import 'package:flutter_components/src/providers/menu_provider.dart';
 import 'package:flutter_components/src/utils/icon_string_util.dart';
 
@@ -17,11 +18,11 @@ class HomePage extends StatelessWidget {
     return FutureBuilder(
       future: menuProvider.cargarData(),
       initialData: [],
-      builder: (contex, AsyncSnapshot<List<dynamic>> snapshot) {
+      builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
         // print('builder');
         // print(snapshot.data);
         return ListView(
-          children: _listaItems(snapshot.data),
+          children: _listaItems(snapshot.data, context),
         );
       },
     );
@@ -31,7 +32,7 @@ class HomePage extends StatelessWidget {
     // );
   }
 
-  List<Widget> _listaItems(List<dynamic> data) {
+  List<Widget> _listaItems(List<dynamic> data, BuildContext context) {
     final List<Widget> opciones = [];
 
     data.forEach((opt) {
@@ -39,7 +40,10 @@ class HomePage extends StatelessWidget {
         title: Text(opt['texto']),
         leading: getIcon(opt['icon']),
         trailing: Icon(Icons.keyboard_arrow_right),
-        onTap: () => {},
+        onTap: () {
+          final route = MaterialPageRoute(builder: (context) => AlertPage());
+          Navigator.push(context, route);
+        },
       );
       opciones..add(widgetTemp)..add(Divider());
     });
