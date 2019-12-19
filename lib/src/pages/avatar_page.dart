c4ff349ce0ca72ed5094dee960b0af3b4f46323d
Javier Claros <jclaros@afp-futuro.com>
2019-12-19 12:46:24 -0400

    Avatar terminado

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -6,9 +6,31 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.all(5.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://img.vixdata.io/pd/jpg-large/es/sites/default/files/r/rip-stan-lee.jpg'),
+              radius: 25.0,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('SL'),
+              backgroundColor: Colors.brown,
+            ),
+          )
+        ],
       ),
       body: Center(
-        child: Text('Hi again'),
+        child: FadeInImage(
+          image: NetworkImage(
+              'https://e00-marca.uecdn.es/assets/multimedia/imagenes/2018/11/28/15433959641322.jpg'),
+          placeholder: AssetImage('data/assets/jar-loading.gif'),
+          fadeInDuration: Duration(milliseconds: 200),
+        ),
       ),
     );
   }
