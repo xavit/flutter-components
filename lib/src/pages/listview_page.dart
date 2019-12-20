4763db3d1d79fe2cc830ca047d07ba854f9b67a5
Javier Claros <jclaros@afp-futuro.com>
2019-12-20 17:20:04 -0400

    ListView Builder e Infinite Scroll, agregue loading, y efecto de scroll up para el final de la lista cargada

--- lib/src/pages/listview_page.dart
+++ lib/src/pages/listview_page.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class ListaPage extends StatefulWidget {
   @override
@@ -6,28 +9,102 @@ class ListaPage extends StatefulWidget {
 }
 
 class _ListaPageState extends State<ListaPage> {
-  List<int> _listaNumeros = [10, 20, 30, 40, 50];
+  ScrollController _scrollController = new ScrollController();
+
+  List<int> _listaNumeros = new List();
+  int _ultimoItem = 0;
+  bool _isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _agregar10();
+
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+        // Si la posición actual es igual a la máxima de la pantalla, hacer algo
+        // _agregar10();
+        fetchData();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _scrollController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Listview Builder'),
-      ),
-      body: _crearLista(),
-    );
+        appBar: AppBar(
+          title: Text('Listview Builder'),
+        ),
+        body: Stack(
+          children: <Widget>[_crearLista(), _crearLoading()],
+        ));
   }
 
   Widget _crearLista() {
     return ListView.builder(
+      controller: _scrollController,
       itemCount: _listaNumeros.length,
       itemBuilder: (BuildContext context, int index) {
         final imagen = _listaNumeros[index];
         return FadeInImage(
           image: NetworkImage('https://picsum.photos/id/$imagen/500/300'),
-          placeholder: AssetImage('data/assets/jar-loading.gif'),
+          placeholder: AssetImage('data/assets/04-spinner.gif'),
+          width: 400.0,
+          height: 247.0,
         );
       },
     );
+  }
+
+  void _agregar10() {
+    for (var i = 0; i < 10; i++) {
+      _ultimoItem++;
+      _listaNumeros.add(_ultimoItem);
+    }
+    setState(() {});
+  }
+
+  Future fetchData() async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    final duration = new Duration(seconds: 2);
+    return new Timer(duration, respuestaHTTP);
+  }
+
+  void respuestaHTTP() {
+    _isLoading = false;
+    _scrollController.animateTo(_scrollController.position.pixels + 100,
+        curve: Curves.fastOutSlowIn, duration: Duration(milliseconds: 250));
+    _agregar10();
+  }
+
+  Widget _crearLoading() {
+    if (_isLoading) {
+      return Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[CircularProgressIndicator()],
+          ),
+          SizedBox(
+            height: 20.0,
+          )
+        ],
+      );
+    } else {
+      return Container();
+    }
   }
 }
